fde3821f2d6424d4602c97e6da84337d0e331c57
Nihan <nihan464@gmail.com>
2026-03-16 12:49:08 +0530

    task1.A rover distance calculation

--- Task_1a.py
+++ Task_1a.py
@@ -1,7 +1,12 @@
+#input starting coordinates x and y of rover
 xi = int(input("Enter the initial coordinates x : "))
 yi = int(input("Enter the initial coordinates y : "))
+
+#input starting coordinates x and y of rover
 xf = int(input("Enter the final coordinates x : "))
 yf = int(input("Enter the final coordinates y : "))
+
+#caculate the distance covered by the rover using the distance formula
 distance =  ((yf-yi)**2+((xf-xi)**2))**0.5
 print("The total distance covered by the rover : " ,distance)
 
